1fd1bf1ef0d3bb53765d8df6122c81a1a4399094
Eric B <ericbieszczad@gmail.com>
2021-10-14 14:48:04 +0200

    bug fixes + using RGB not HSV

--- Oppg4/main.py
+++ Oppg4/main.py
@@ -6,7 +6,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import random 
+
 import time
 import threading 
 
@@ -16,14 +16,14 @@ ev3 = EV3Brick()
 left_motor = Motor(port=Port.A)
 right_motor = Motor(port=Port.B)
 ultrasonic_sensor = UltrasonicSensor(port=Port.S3)
-robot = DriveBase(leftMotor, rightMotor, wheel_diameter=56, axle_track=82.1)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=82.1)
 back_color_sensor = ColorSensor(port=Port.S2)
 front_color_sensor = ColorSensor(port=Port.S1)
 
 
 # Write your program here.
 def main():
-    thread: threading.Thread = threading.Thread(
+    thread = threading.Thread(
         target=ev3.speaker.play_file,
         args=(SoundFile.FANFARE,)
     )
@@ -48,8 +48,8 @@ def main():
         else:
             # Shouldn't be turning when we're going FAAAST AF! xD
             robot.drive(FAST)
-            if not thread.is_alive()
-                thread: threading.Thread = threading.Thread(
+            if not thread.is_alive():
+                thread = threading.Thread(
                     target=ev3.speaker.play_file,
                     args=(SoundFile.FANFARE,)
                 )
@@ -61,11 +61,8 @@ def main():
 
 def get_brightness(color_sensor):
     """Returns the brightness (black - white) from 0 - 100"""
-
-    # .color returnerer HSV-farge hvor V = brightness value fra 0 - 100
-    # H = Hue og er fra 0 - 360 ()
-    # S = Saturation og er fra 0 - 100 (100 = sterk farge, 0 = svart-hvitt)
-    return color_sensor.color().v
+    # https://www.rapidtables.com/convert/color/rgb-to-hsv.html
+    return max(color_sensor.rgb())
     
 
 if __name__ == "__main__":
