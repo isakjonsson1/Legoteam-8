f353079b55917bc1f2f0d7edaf65b9a5c539a5a8
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-10-19 17:13:14 +0200

    fixed files

--- Oppg4/config.py
+++ Oppg4/config.py
@@ -1,6 +1,8 @@
 FAST = -500 # Speed when going fast as fuccboi
-SLOW = -120 # Speed when going slow
-TURN_SPEED = 50 # How fast the robot should 
+SLOW = -170 # Speed when going slow
+TURN_MIN = 10 # How fast the robot should 
+TURN_MAX = 60
+TURN_INC = 2
 
-BLACK_THRESH = 15 # Value under BLACK_THRESH will be considered black (brightness = 0 - 255)
-WHITE_THRESH = 80 # Value over WHITE_THRESH will be considered white (brightness = 0 - 255)
+BLACK_THRESH = 30 # Value under BLACK_THRESH will be considered black (brightness = 0 - 255)
+WHITE_THRESH = 50 # Value over WHITE_THRESH will be considered white (brightness = 0 - 255)
