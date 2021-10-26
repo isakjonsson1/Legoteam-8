2f0b92617883fcc8a8c91a7c73c0d9ee2f548704
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-10-26 23:11:32 +0200

    Added functionality for finding the length of a vector, and calculations related to converting from arc-length to t and curvature

--- Del 2/Curve.py
+++ Del 2/Curve.py
@@ -1,10 +1,11 @@
 from math import atan2
+from numpy import linspace
 
 
 class Curve:
     def __init__(self, points):
         """
-        Creates a Bézier Curve based on four absolute points
+        Creates a Bézier Curve based on four absolute points.
         """
         if len(points) != 4:
             raise ValueError("You can only generate a curve from 4 points")
@@ -16,30 +17,103 @@ class Curve:
         self.c = self.points[1] - self.points[0]
         self.d = self.points[0]
 
+        n = 100
+        self.LUT = self.generate_LUT(n)
+        self.dt = 1 / n
+
     def get_pos(self, t):
         """
         Returns a position on the curve based a t value [0, 1]
-        where 0 is the start of the curve and 1 is the end of the curve
+        where 0 is the start of the curve and 1 is the end of the curve.
         """
         return t ** 3 * self.a + 3 * t ** 2 * self.b + 3 * t * self.c + self.d
 
     def get_vel(self, t):
         """
         Returns the velocity at a given t value [0, 1] (the first order derivative)
-        where 0 is the start of the curve and 1 is the end of the curve
+        where 0 is the start of the curve and 1 is the end of the curve.
         """
         return 3 * t ** 2 * self.a + 6 * t * self.b + 3 * self.c
 
     def get_acc(self, t):
         """
-        Returns the acceleration at a give t value [0, 1] (the second order derivative)
-        where 0 is the start of the curve and 1 is the end of the curve
+        Returns the acceleration at a given t value [0, 1] (the second order derivative)
+        where 0 is the start of the curve and 1 is the end of the curve.
         """
         return 6 * t * self.a + 6 * self.b
 
     def get_jerk(self):
         """Returns the jerk throughout the curve (the third order derivative)"""
         return 6 * self.a
+
+    def get_curvature(self, t):
+        """
+        Returns the curvature of the curve at a given t value [0, 1]
+        where 0 is the start of the curve and 1 is the end of the curve.
+
+        The curvature is the inverse of the radius.
+        """
+
+        vel = self.get_vel(t)
+        acc = self.get_acc(t)
+
+        return (vel.x * acc.y - vel.y * acc.x) / ((vel.x ** 2 + vel.y ** 2) ** (3 / 2))
+
+    def generate_LUT(self, n=100):
+        """Generates a look-up table to convert from distance to t values [0, 1]"""
+
+        # Initiates a new dict as an empty look-up table
+        LUT = dict()
+
+        # Samples of t in the interval [0, 1]
+        ts = linspace(0, 1, n + 1)
+
+        L = 0
+        LUT[L] = 0
+
+        lst = self.get_pos(ts[0])
+        for t in ts[1:]:
+            cur = self.get_pos(t)
+            l = len(cur - lst)
+            L += l
+            LUT[L] = t
+
+    def bin_search_LUT(self, L):
+        """
+        Returns an two values. The first one is the lower bound of the value from
+        the look-up table (inclusive), and the other one is the upper bound (exclusive)
+        """
+        keys = list(self.LUT.keys())
+
+        # Min and max index
+        min_i, max_i = 0, len(keys) - 1
+        # Min and max value
+        min_v, max_v = 0, keys[max_i]
+
+        while True:
+            mid_i = (min_i + max_i) // 2
+            mid_v = keys[mid_i]
+
+            if L < mid_v:
+                max_i = mid_i
+                max_v = mid_v
+            else:
+                min_i = mid_i
+                min_v = mid_v
+
+            if min_i + 1 == max_i:
+                return min_v, max_v
+
+    def find_t(self, L):
+        """Finds a t given a an arc-length L"""
+        if L < 0 or len(self) <= L:
+            raise ValueError("L needs to greater than 0 and under the total arc-length")
+
+        min_L, max_L = self.bin_search_LUT(L)
+        min_t, max_t = self.LUT[min_L], self.LUT[max_L]
+
+        ratio = (L - min_L) / (max_L - min_L)
+        return (max_t - min_t) * ratio + min_t
 
     def get_endpoint(self):
         """Returns the absolute position where the curve ended"""
@@ -67,6 +141,9 @@ class Curve:
             points_abs.append(start + point)
 
         return cls(points_abs)
+
+    def __len__(self):
+        return self.LUT.keys()[-1]
 
 
 if __name__ == "__main__":
--- Del 2/Point.py
+++ Del 2/Point.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -30,6 +33,9 @@ class Point:
             return False
 
         return self.x == other.x and self.y == other.y
+
+    def __len__(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
 
     def __str__(self):
         return f"({self.x}, {self.y})"
