e5f512e07c8c9db7420df267fc230875b02d2df0
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2021-10-28 01:02:30 +0200

    linting

--- Del 2/app/utils/plotting.py
+++ Del 2/app/utils/plotting.py
@@ -1,11 +1,12 @@
+"""Utility functions for plotting"""
 from matplotlib import pyplot as plt
 import numpy as np
 
-
 def plot_curve(curve):
-    ts = np.linspace(0, 1)
-    points = [curve.get_pos(t) for t in ts]
+    """Plots a given curve"""
+    t_values = np.linspace(0, 1)
+    points = [curve.get_pos(t) for t in t_values]
+
     x = [p.x for p in points]
     y = [p.y for p in points]
-
     plt.plot(x, y)
