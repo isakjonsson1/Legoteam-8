d4b16148829a8c2335f83bcdfe272a6228e31b46
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-10-28 11:14:12 +0200

    added tests for all curves

--- Del 2/tests/test_curves.py
+++ Del 2/tests/test_curves.py
@@ -1,30 +1,69 @@
-from math import sqrt
+import pytest
+from app.abc.non_linear_curve import NonLinearCurve
 
 from app.curves import Line
+from app.curves import QuadraticCurve
 from app.curves import NthDegreeCurve
+from app.curves.cubic_curve import CubicCurve
 from app.point import Point as P
 
+points = [P(0.25, 0), P(0, 1), P(1, 1), P(0.75, 0), P(0, -1), P(1, -1)]
+
+
 def test_line():
-    points = [
-        P(0, 0),
-        P(1, 1)
-    ]
-    l = Line(points)
-    assert l.length() == sqrt(2)
-    assert l.get_t(1) == 1 / l.length()
-    assert l.get_t(2) == 2 / l.length()
+    l = Line(points[1:3])
+    assert l.length() == 1
+    assert l.get_t(0.5) == 0.5 / l.length()
+
+    with pytest.raises(ValueError):
+        l.get_t(2)
+        l.get_t(-1)
+
+    _test_curve(l)
+
+
+def test_quadratic_curve():
+    c = QuadraticCurve(points[:3])
+
+    with pytest.raises(ValueError):
+        QuadraticCurve(points[:2])
+        QuadraticCurve(points[0:4])
+
+    _test_curve(c)
+
+
+def test_quadratic_curve():
+    c = CubicCurve(points[:4])
+
+    with pytest.raises(ValueError):
+        CubicCurve(points[:3])
+        QuadraticCurve(points[0:5])
+
+    _test_curve(c)
+
 
 def test_nth_degree_curve():
-    points = [
-        P(0.25, 0),
-        P(0, 1),
-        P(1, 1),
-        P(0.75, 0),
-        P(0, -1),
-        P(1, -1)
-    ]
     c = NthDegreeCurve(points)
-    # More testing
 
-    # Tanken her er at vi kan kjøre tests for å sjekke om endringer ødelegger
-    # funksjonaliteten i koden vår
+    with pytest.raises(ValueError):
+        NthDegreeCurve(points[0:3])
+
+    _test_curve(c)
+
+
+def _test_curve(curve):
+    assert curve.length() > 0
+    assert curve.get_t(0) == 0
+    assert curve.get_pos(1) == curve.get_endpoint()
+    assert isinstance(curve.get_pos(0.5), P)
+    assert isinstance(curve.get_vel(0.5), P)
+    assert isinstance(curve.get_acc(0.5), P)
+
+    with pytest.raises(ValueError):
+        curve.get_t(-1)
+        curve.get_t(curve.length())
+
+    if isinstance(curve, NonLinearCurve):
+        # Checks that the keys in the the look-up table are sorted
+        keys = list(curve.LUT.keys())
+        assert all(keys[i] <= keys[i + 1] for i in range(len(keys) - 1))
