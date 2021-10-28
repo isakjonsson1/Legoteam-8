0f32f9c102dc1efb629ae5fd90d3c0dee6b963a0
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-10-28 11:18:16 +0200

    Added check for arc length in get_t function

--- Del 2/app/abc/linear_curve.py
+++ Del 2/app/abc/linear_curve.py
@@ -1,14 +1,19 @@
 from app.abc import Curve
 
+
 class LinearCurve(Curve):
     """
     An abstract class for linear curves. This type of curve assumes that
     atraversed distance is proportional at any point on the curve
     """
+
     def __init__(self, points):
         """Creates a curve given a list of points"""
         super().__init__(points)
 
     def get_t(self, L):
         """Finds a t given a an arc-length L"""
+        if L < 0 or self.length() <= L:
+            raise ValueError("L needs to greater than 0 and under the total arc-length")
+
         return L / self.length()
