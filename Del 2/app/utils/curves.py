fad25c493a45d41752fbc13564adf14b35acee76
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-11-05 11:13:13 +0100

    Working parser --all files

--- Del 2/app/utils/curves.py
+++ Del 2/app/utils/curves.py
@@ -3,8 +3,16 @@ from app.curves import Line, QuadraticCurve, CubicCurve, NthDegreeCurve
 from app.abc.curve import Curve
 
 
-def make_curve(points):
-    """Creates a bezier curve based on any number of points"""
+def make_curve(points, relative=False):
+    """
+    Creates a bezier curve based on any number of points.
+
+    If relative is set to true, the points are converted form relative
+    points to absolute using the first point in the list as the base.
+    """
+    if relative:
+        points = Curve.convert_rel_points_to_abs_points(points[0], points[1:])
+
     if len(points) == 2:
         return Line(points)
     if len(points) == 3:
@@ -15,13 +23,3 @@ def make_curve(points):
         return NthDegreeCurve(points)
 
     raise ValueError("The number of points needs to be higher than one")
-
-
-def make_curve_relative(points):
-    """
-    Creates a bezier curve based on any number of points.
-    The points gievn in a relative manner where the first point is given in absolute coordinates,
-    while the other points are calculated based on it.
-    """
-    points = Curve.convert_rel_points_to_abs_points(points[0], points[1:])
-    return make_curve(points)
--- Del 2/run.py
+++ Del 2/run.py
@@ -7,22 +7,28 @@ from matplotlib import pyplot as plt
 from app.point import Point
 from app.curves import Line
 from app.curves import NthDegreeCurve
+from app.svg.parsing import parse_svg
 from app.utils.plotting import plot_curve, plot_path
-from matplotlib import pyplot as plt
 
 from app.svg import Path
 
 
 def main():
-    path = Path(Point(0, 3))
-
-    print(Point.from_list([1, 2]))
-
-    path.append_curve([Point(0, 1), Point(2, 3), Point(1, 3)])
-    path.append_curve([Point(2, 3), Point(3, 5), Point(3, 3)])
-    path.append_curve([Point(1, 1)])
-    plot_path(path)
+    paths = parse_svg(r"app\svg\sample_svgs\smiley.svg")
+    print(len(paths))
+    for path in paths:
+        plot_path(path)
     plt.show()
+
+    # path = Path(Point(0, 3))
+
+    # print(Point.from_list([1, 2]))
+
+    # path.append_curve([Point(0, 1), Point(2, 3), Point(1, 3)])
+    # path.append_curve([Point(2, 3), Point(3, 5), Point(3, 3)])
+    # path.append_curve([Point(1, 1)])
+    # plot_path(path)
+    # plt.show()
 
     # """Program entrypoint - Here comes the main logic"""
     # points = [P(0.25, 0), P(0, 1), P(1, 1), P(0.75, 0), P(0, -1), P(1, -1)]
