5b6d8de07ae71e2767272793208de193182a22b7
Eric B <ericbieszczad@gmail.com>
2021-11-11 21:18:48 +0100

    utf8 encoding

--- Del 2/app/utils/logging.py
+++ Del 2/app/utils/logging.py
@@ -26,7 +26,7 @@ class Logger:
     CRITICAL = 50
     def __init__(self, 
                  name=__name__,
-                 outputs={ sys.stdout: 10, open("latest.log", "a"): 20 },
+                 outputs={ sys.stdout: 10, open("latest.log", "a", encoding="UTF-8"): 20 },
                  log_format="%(asctime)s |:| %(name)s:%(levelname)-8s |:| %(message)s",
                  silent_log_errors=True):
         self.name = name
