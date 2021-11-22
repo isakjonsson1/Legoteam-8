af6c60c6b5967b1262e72759f3fb9c84c0ebb299
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-11-22 16:54:57 +0100

    Final commit!

--- Del 2/robot/calibrate.py
+++ Del 2/robot/calibrate.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env pybricks-micropython
 """Functions used to calibrate the robot"""
+import math
 
 from robot import Robot
+from robot.config import SPEED
 
 bot = Robot(scale=1)
 
 
 def main():
     """Start function"""
-    threesixty()
+    bot.engage_pen()
+    circle()
+    bot.lift_pen()
 
 
-def square(length):
+def square(length=100):
     """Makes the robot drive in a square with sides length milimeters long"""
     bot.drive_base.straight(length)
     bot.drive_base.turn(90)
@@ -31,6 +35,15 @@ def drive(length):
 def threesixty():
     """Turns 360 degrees"""
     bot.drive_base.turn(360)
+
+
+def circle(radius=200):
+    """Turns in a circle"""
+    bot.drive_base.reset()
+    bot.drive_base.drive(SPEED, math.degrees(SPEED / radius))
+    while bot.drive_base.distance() < (2 * math.pi * radius):
+        pass
+    bot.drive_base.stop()
 
 
 if __name__ == "__main__":
